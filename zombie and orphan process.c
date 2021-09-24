c063b2a44bded215a6a8388f9688f681323cee89
ln10-cloud <54163495+ln10-cloud@users.noreply.github.com>
2021-09-24 04:59:09 -0400

    Update zombie and orphan process.c

--- zombie and orphan process.c
+++ zombie and orphan process.c
@@ -11,6 +11,8 @@ int main ()
   int child_status;
 
   child_pid = fork ();
+  fprintf(stderr,"child process - %d\n", getpid());
+	
   if (child_pid > 0) {
     // parent process will sleep for 30 seconds and exit, without a call to wait()
     fprintf(stderr,"parent process - %d\n", getpid());    
